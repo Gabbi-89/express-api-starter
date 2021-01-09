a3a43e715c72e4850c2a88390dc6f2a391266c95
Gabbi-89 <gabriellabolin@hotmail.com>
2021-01-09 15:35:08 +0100

    Codealong session for Authentication

--- server.js
+++ server.js
@@ -1,10 +1,43 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
+import mongoose from 'mongoose';
+import crypto from 'crypto';
+import bcrypt from 'bcrypt-nodejs';
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/auth"
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.Promise = Promise
+
+const User = mongoose.model('User', {
+  name: {
+    type: String,
+    unique: true
+  },
+  email: {
+    type: String,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  accessToken: {
+    type: String,
+    default: () => crypto.randomBytes(128).toString('hex')
+  }
+});
+
+const authenticateUser = async (req, res, next) => {
+  const user = await User.findOne({ accessToken: req.header('Authorization') });
+  if (user) {
+    req.user = user;
+    next();
+  } else {
+    res.status(401).json({ loggedOut: true })
+  }
+}
+
 //   PORT=9000 npm start
 const port = process.env.PORT || 8080
 const app = express()
@@ -17,6 +50,38 @@ app.use(bodyParser.json())
 app.get('/', (req, res) => {
   res.send('Hello world')
 })
+
+// Registration endpoint
+app.post('/users', async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    // DO NOT store plaintext passwords
+    const user = new User({ name, email, password: bcrypt.hashSync(password) });
+    // Saves user to the database
+    user.save();
+    res.status(201).json({ id: user._id, accessToken: user.accessToken });
+  } catch (err) {
+    res.status(400).json({ message: 'Could not create user', errors: err.errors });
+  }
+});
+
+// Need to authenticate user first to see if user is logged in, 
+// if yes the get request executes
+app.get('/secrets', authenticateUser);
+app.get('/secrets', (req, res) => {
+  res.json({ secret: 'This is a super secret message' });
+});
+
+// Possibility for the user to log in
+app.post('/sessions', async (req, res) => {
+  // User to log in with email - to be checked in the database
+  const user = await User.findOne({ email: req.body.email });
+  if (user && bcrypt.compareSync(req.body.password, user.password)) {
+    res.json({ userId: user._id, accessToken: user.accessToken });
+  } else {
+    res.json({ notFound: true });
+  }
+});
 
 // Start the server
 app.listen(port, () => {
